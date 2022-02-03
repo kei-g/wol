bbd060066b55fe16394cb48dd034536a7712b77c
kei-g <km.8k6ce+github@gmail.com>
2022-02-03 20:54:22 +0900

    :hammer: Add flags, -Wall -Werror -Wextra -pedantic, to build for x86_64-w64-mingw32
    Signed-off-by: kei-g <km.8k6ce+github@gmail.com>

--- conf/x86_64-w64-mingw32/build.sh
+++ conf/x86_64-w64-mingw32/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-clang --target=x86_64-w64-mingw32 -Oz -Wl,-s -flto -fuse-ld=lld -lws2_32 -o wol.exe wol.c
+clang --target=x86_64-w64-mingw32 -Oz -Wall -Werror -Wextra -Wl,-s -flto -fuse-ld=lld -lws2_32 -o wol.exe -pedantic wol.c
