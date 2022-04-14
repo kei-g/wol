b82d152228840cb081d1db796e109739b6b2a1f2
kei-g <km.8k6ce+github@gmail.com>
2022-04-14 21:23:11 +0900

    :whale: Separate arguments to multiple lines
    Signed-off-by: kei-g <km.8k6ce+github@gmail.com>

--- conf/x86_64-w64-mingw32/build.sh
+++ conf/x86_64-w64-mingw32/build.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-clang --target=x86_64-w64-mingw32 -Oz -Wall -Werror -Wextra -Wl,-s -flto -fuse-ld=lld -lws2_32 -o wol.exe -pedantic wol.c
+clang \
+  --target=x86_64-w64-mingw32 \
+  -Oz \
+  -Wall \
+  -Werror \
+  -Wextra \
+  -Wl,-s \
+  -flto \
+  -fuse-ld=lld \
+  -lws2_32 \
+  -o wol.exe \
+  -pedantic \
+  wol.c
